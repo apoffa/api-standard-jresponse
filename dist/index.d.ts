2f0262914d8250e842316a5007f787f9dd9f716c
Ciak <alessandro.p@oneabee.com>
2019-02-20 11:03:35 +0100

    Small fix

---
+++ dist/index.d.ts
@@ -0,0 +1,15 @@
+export declare const setJResponse: (req: any, res: any, next: any) => any;
+export declare class JResponse {
+    private readonly res;
+    private status;
+    private response;
+    constructor(res: any);
+    static success(res: any, data?: any, status?: number): any;
+    static errors(res: any, errors?: any, status?: number): any;
+    static build(success: boolean, data?: any, errors?: any): any;
+    send(success: boolean, data?: any, error?: any): any;
+    sendSuccess(data?: any): any;
+    sendErrors(errors?: any, status?: number): any;
+    appendError(error?: any, status?: number): void;
+    appendData(data?: any, status?: number): void;
+}
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,13 @@
-export const setJResponse = (req: any, res: any, next: any) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setJResponse = function (req, res, next) {
     console.log("arrive here");
     res.JRes = new JResponse(res);
     return next();
 };
-
-export class JResponse {
-
-    private readonly res: any;
-    private status = 200;
-    private response: any;
-
-    constructor(res: any) {
+var JResponse = /** @class */ (function () {
+    function JResponse(res) {
+        this.status = 200;
         this.res = res;
         this.response = {
             success: false,
@@ -19,23 +16,26 @@ export class JResponse {
             errors: []
         };
     }
-
-    static success(res: any, data: any = [], status = 200) {
+    JResponse.success = function (res, data, status) {
+        if (data === void 0) { data = []; }
+        if (status === void 0) { status = 200; }
         return res.status(status).json(JResponse.build(true, Array.isArray(data) ? data : [data]));
-    }
-
-    static errors(res: any, errors: any = [], status = 400) {
+    };
+    JResponse.errors = function (res, errors, status) {
+        if (errors === void 0) { errors = []; }
+        if (status === void 0) { status = 400; }
         return res.status(status).json(JResponse.build(false, null, Array.isArray(errors) ? errors : [errors]));
-    }
-
-    static build(success: boolean, data: any = [], errors: any = []) {
+    };
+    JResponse.build = function (success, data, errors) {
+        if (data === void 0) { data = []; }
+        if (errors === void 0) { errors = []; }
         if (data !== null && !Array.isArray(data)) {
             throw new Error("'data' field must be Array (if set)");
         }
         if (errors !== null && !Array.isArray(errors)) {
             throw new Error("'error' field must be Array (if set)");
         }
-        const response: any = {success: true, count: 0, data: [], errors: []};
+        var response = { success: true, count: 0, data: [], errors: [] };
         if (errors !== null) {
             response.success = false;
             response.errors = errors;
@@ -45,18 +45,20 @@ export class JResponse {
             response.count = response.data.length;
         }
         return response;
-    }
-
-    send(success: boolean, data: any = [], error: any = []) {
+    };
+    JResponse.prototype.send = function (success, data, error) {
+        if (data === void 0) { data = []; }
+        if (error === void 0) { error = []; }
         if (this.res === undefined || this.res === null) {
             throw new Error("Response object not initialized. You probably haven't set the middleware correctly => app.use(setJResponse());");
         }
         this.status = this.status ? this.status : 200;
-        this.response = this.response ? this.response : {success: true, count: 0, data: [], errors: []};
+        this.response = this.response ? this.response : { success: true, count: 0, data: [], errors: [] };
         if (data !== null) {
             if (Array.isArray(data) && data.length > 0) {
                 this.response.data = data;
-            } else if (!Array.isArray(data)) {
+            }
+            else if (!Array.isArray(data)) {
                 this.response.data.push(data);
             }
             this.response.count = this.response.data.length;
@@ -64,40 +66,46 @@ export class JResponse {
         if (error !== null) {
             if (Array.isArray(error) && error.length > 0) {
                 this.response.errors = error;
-            } else if (!Array.isArray(data)) {
+            }
+            else if (!Array.isArray(data)) {
                 this.response.errors.push(error);
             }
         }
         return this.res.status(this.status).json(this.response);
-    }
-
-    sendSuccess(data: any = null) {
+    };
+    JResponse.prototype.sendSuccess = function (data) {
+        if (data === void 0) { data = null; }
         if (data !== null) {
             Array.isArray(data) ? this.response.data = this.response.data.concat(data) : this.response.data.push(data);
         }
         this.response.success = true;
         return this.send(true);
-    }
-
-    sendErrors(errors: any = null, status = 400) {
+    };
+    JResponse.prototype.sendErrors = function (errors, status) {
+        if (errors === void 0) { errors = null; }
+        if (status === void 0) { status = 400; }
         if (errors !== null) {
             Array.isArray(errors) ? this.response.errors = this.response.errors.concat(errors) : this.response.errors.push(errors);
         }
         this.status = status;
         return this.send(false);
-    }
-
-    appendError(error: any = null, status = 400) {
+    };
+    JResponse.prototype.appendError = function (error, status) {
+        if (error === void 0) { error = null; }
+        if (status === void 0) { status = 400; }
         this.status = status;
         if (error !== null) {
             Array.isArray(error) ? this.response.errors = this.response.errors.concat(error) : this.response.errors.push(error);
         }
-    }
-
-    appendData(data: any = null, status = 200) {
+    };
+    JResponse.prototype.appendData = function (data, status) {
+        if (data === void 0) { data = null; }
+        if (status === void 0) { status = 200; }
         this.status = status;
         if (data !== null) {
             Array.isArray(data) ? this.response.data = this.response.data.concat(data) : this.response.data.push(data);
         }
-    }
-}
+    };
+    return JResponse;
+}());
+exports.JResponse = JResponse;
